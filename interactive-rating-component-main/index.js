9cc8dc2f6177ad495224332be3cbbb743ac19070
Mohsen Sobhaniasl <46324402+Sobhaniasl@users.noreply.github.com>
2022-11-10 20:46:58 +0100

    Add files via upload

---
+++ interactive-rating-component-main/index.js
@@ -0,0 +1,26 @@
+const mainContainer = document.querySelector(".main-container");
+const thanksContainer = document.querySelector(".thank-you");
+const submitButton = document.getElementById("submit");
+const rateAgain = document.getElementById("rate-again");
+const rating = document.getElementById("rating");
+const rates = document.querySelectorAll(".btn");
+
+
+submitButton.addEventListener("click", myFunction);
+
+function myFunction() {
+  thanksContainer.classList.remove("hidden")
+  mainContainer.style.display = "none"
+}
+
+rateAgain.addEventListener("click", myFunction2);
+function myFunction2() {
+  thanksContainer.classList.add("hidden")
+  mainContainer.style.display = "block"
+}
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", ()=> {
+    rating.innerHTML = rate.innerHTML
+  })
+})
