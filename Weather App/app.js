f93c5a725d4b89d0612d96a887f4d0fed8e965c0
Mohsen Sobhaniasl <46324402+Sobhaniasl@users.noreply.github.com>
2023-02-01 10:26:01 +0100

    Update JS

--- Weather App/app.js
+++ Weather App/app.js
@@ -49,7 +49,7 @@ function showError(error){
 function getWeather(latitude, longitude){
     let api = 
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-    console.log(api);
+  
     fetch(api)
     .then(function(respone){
         let data = respone.json();
